73fb73f0ebdf6fade51d650899efe2af8bf989a2
Leonardo Fuentes Claros <51714281+leonardoGit99@users.noreply.github.com>
2024-06-03 22:10:58 -0400

    Initial commit

---
+++ src/main/java/org/umss/sis_umss/SisUmssApplication.java
@@ -0,0 +1,13 @@
+package org.umss.sis_umss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisUmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisUmssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/umss/sis_umss/SisUmssApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umss.sis_umss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisUmssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
