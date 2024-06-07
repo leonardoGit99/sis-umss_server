84124a9ce77032c789b34c50637e2dc96bd53221
Leonardo Fuentes Claros <51714281+leonardoGit99@users.noreply.github.com>
2024-06-06 20:35:41 -0400

    Complete the subjects endpoints

--- src/main/java/org/umss/sis_umss/controllers/SubjectController.java
+++ src/main/java/org/umss/sis_umss/controllers/SubjectController.java
@@ -16,6 +16,12 @@ import java.util.UUID;
 @RequestMapping("/subjects")
 public class SubjectController {
 
+    //Get one subject
+    @GetMapping("/{subjectUuid}")
+    public SubjectDTO getOneSubject(@PathVariable String subjectUuid) {
+        return new SubjectDTO(subjectUuid, "Intro a la programacion", "INTRO-CODE");
+    }
+
     //Get all groups by one subject
     @GetMapping("/{subjectUuid}/groups")
     public List<GroupDTO> getAll() {
@@ -26,14 +32,6 @@ public class SubjectController {
         return Collections.singletonList(groupDTO);
     }
 
-    //Create a group into one subject
-    @PostMapping("/{subjectUuid}/groups")
-    public GroupDTO create(@PathVariable String subjectUuid, @RequestBody GroupDTO group) {
-        group.setUuid(UUID.randomUUID().toString());
-        group.setProfessor(new ProfessorDTO(group.getProfessor().getUuid(), "Juan Carlos", "Garcia"));
-        group.setSubject(new SubjectDTO(group.getSubject().getUuid(), "Intro a la programacion", "INTRO-CODE"));
-        return group;
-    }
 
     // Get one group by one subject
     @GetMapping("/{subjectUuid}/groups/{groupUuid}")
@@ -43,5 +41,33 @@ public class SubjectController {
         groupDTO.setProfessor(new ProfessorDTO(UUID.randomUUID().toString(), "Juan Carlos", "Garcia"));
         groupDTO.setSchedule(Collections.singletonList(new ScheduleDTO(DayOfWeek.MONDAY, "6:45", "8:15")));
         return groupDTO;
+    }
+
+    //Create a group into one subject
+    @PostMapping("/{subjectUuid}/groups")
+    public GroupDTO createGroup(@PathVariable String subjectUuid, @RequestBody GroupDTO group) {
+        group.setUuid(UUID.randomUUID().toString());
+        group.setProfessor(new ProfessorDTO(group.getProfessor().getUuid(), "Juan Carlos", "Garcia"));
+        group.setSubject(new SubjectDTO(group.getSubject().getUuid(), "Intro a la programacion", "INTRO-CODE"));
+        return group;
+    }
+
+    //Create subject
+    @PostMapping
+    public SubjectDTO createSubject(@RequestBody SubjectDTO subject) {
+        subject.setUuid(UUID.randomUUID().toString());
+        return subject;
+    }
+
+    // Update subject
+    @PutMapping("{subjectUuid}")
+    public SubjectDTO updateSubject(@PathVariable String subjectUuid, @RequestBody SubjectDTO subject) {
+        return new SubjectDTO(UUID.randomUUID().toString(), "Intro a la programacion 2", "INTRO-CODE");
+    }
+
+    @DeleteMapping("{subjectUuid}")
+    public SubjectDTO deleteSubject(@PathVariable String subjectUuid) {
+        //Logic: if subject doesn't belong  a group, so...
+        return new SubjectDTO(subjectUuid, "Intro a la programacion ", "INTRO-CODE");
     }
 }
