6f4274b7e76ecc7823f5854a3d15579c73e826db
Leonardo Fuentes Claros <51714281+leonardoGit99@users.noreply.github.com>
2024-06-06 21:54:42 -0400

    Add groups endpoints

--- src/main/java/org/umss/sis_umss/controllers/GroupController.java
+++ src/main/java/org/umss/sis_umss/controllers/GroupController.java
@@ -1,9 +1,20 @@
 package org.umss.sis_umss.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.umss.sis_umss.dtos.GroupDTO;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/groups")
 public class GroupController {
+
+    // Delete group
+    @DeleteMapping("/{groupUuid}")
+    public GroupDTO delete(@PathVariable String groupUuid) {
+        return new GroupDTO(groupUuid, "Grupo-1A-CODE", "1A");
+    }
 }
